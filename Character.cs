b1146402ca3f9ea2c0fd0e594ee882fa895a0879
Henrik Vincents Poulsen <hpo@tec.dk>
2023-01-24 10:50:48 +0100

    Add project files.

---
+++ Character.cs
@@ -0,0 +1,39 @@
+﻿namespace H1W2D2RPG
+{
+    internal class Character
+    {
+        //Because the method that uses this array is static, we also make the array static.
+        private static string[] characterClass = { "Warrior", "Wizard", "Priest", "Rouge", "Summoner", "Druid", "Ranger" };
+
+        public string Name { get; set; } = "NoNameYet";
+        public string CharacterClass { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public Character()
+        {
+            GetRandomBirthday();
+        }
+
+        private void GetRandomBirthday()
+        {
+            Random random = new Random();
+            DateTime start = new DateTime(1020, 1, 1);
+            DateTime end = new DateTime(1120, 1, 1);
+            int range = (end - start).Days;
+            int days = random.Next(range);
+            Birthday = start.AddDays(days);
+        }
+
+        //Static method runs on class and not on object
+        public static string GetCharacterClassRandomly()
+        {
+            Random random = new Random();
+            int r = random.Next(characterClass.Length);
+            string cc = characterClass[r];
+            return cc;
+
+            //return characterClass[new Random().Next(characterClass.Length)];
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using H1W2D2RPG;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Character firstChar = new Character();
+firstChar.Name = "EtEllerAndet";
+
+//The method is static, so it runs on the class and not the object
+firstChar.CharacterClass = Character.GetCharacterClassRandomly();
+
+Console.WriteLine(firstChar.Name);
